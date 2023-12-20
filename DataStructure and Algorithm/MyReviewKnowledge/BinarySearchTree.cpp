7199d2e8c4ff07d9924c1d0bea51be7185d019da
Hieu Bui <hieubv1306@gmail.com>
2023-12-20 23:56:13 +0700

    Review binary search tree.

---
+++ DataStructure and Algorithm/MyReviewKnowledge/BinarySearchTree.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cmath>
+using namespace std;
+
+struct Node
+{
+    double data;
+    Node *pLeft;
+    Node *pRight;
+};
+
+struct Tree
+{
+    Node *pRoot;
+};
+
+Node *initNode(double data)
+{
+    Node *newNode = new Node;
+    newNode->data = data;
+    newNode->pLeft = NULL;
+    newNode->pRight = NULL;
+    return newNode;
+}
+
+void initTree(Tree &tree)
+{
+    tree.pRoot = NULL;
+}
+
+void addNode(Tree &tree, Node *newNode)
+{
+    if (tree.pRoot == NULL)
+    {
+        tree.pRoot = newNode;
+        return;
+    }
+    Node *pGoTo = tree.pRoot;
+    Node *pLocation = NULL;
+    while (pGoTo != NULL)
+    {
+        pLocation = pGoTo;
+        if (newNode->data < pGoTo->data)
+        {
+            pGoTo = pGoTo->pLeft;
+        }
+        if (newNode->data > pGoTo->data)
+        {
+            pGoTo = pGoTo->pRight;
+        }
+    }
+    if (newNode->data < pLocation->data)
+    {
+        pLocation->pLeft = newNode;
+    }
+    else if (newNode->data > pLocation->data)
+    {
+        pLocation->pRight = newNode;
+    }
+}
+
+void printBinaryTree(Tree tree)
+{
+
+}
+
+bool find(Tree tree, double keyword)
+{
+
+    return false;
+}
+
+
+int main(int argc, char const *argv[])
+{
+    return 0;
+}
--- DataStructure and Algorithm/MyReviewKnowledge/HashTable.cpp
+++ DataStructure and Algorithm/MyReviewKnowledge/HashTable.cpp
@@ -4,38 +4,100 @@
 #include <cmath>
 using namespace std;
 
+#define MAX_BUCKET 5
 struct Node
 {
     double data;
     Node *pNext;
 };
 
-struct HashTable
+struct Bucket
 {
-    Node *pFront;
-    Node *pRear;
+    Node *pHead;
+    Node *pTail;
 };
 
-Node *initNode(double data)
+struct HashTable
+{
+    Bucket buckets[MAX_BUCKET];
+};
+
+Node *initNode(double value)
 {
     Node *newNode = new Node;
-    newNode->data = data;
+    newNode->data = value;
     newNode->pNext = NULL;
     return newNode;
 }
 
-void initHashTable(HashTable &newHashTable)
+void initBucket(Bucket &newBucket)
 {
-    newHashTable.pFront = NULL;
-    newHashTable.pRear = NULL;
+    newBucket.pHead = NULL;
+    newBucket.pTail = NULL;
 }
 
+void initHashTable(HashTable &hashTable)
+{
+    for (int i = 0; i < MAX_BUCKET; i++)
+    {
+        initBucket(hashTable.buckets[i]);
+    }
+}
 
+int hashFunction(double value)
+{
+    return (int)value % MAX_BUCKET;
+}
 
+void addTail(Bucket &bucket, Node *newNode)
+{
+    if (bucket.pHead == NULL)
+    {
+        bucket.pHead = bucket.pTail = newNode;
+    }
+    else
+    {
+        bucket.pTail->pNext = newNode;
+        bucket.pTail = newNode;
+    }
+}
+
+void add(HashTable &hashTable, Node *newNode) {
+    int index = hashFunction(newNode->data);
+    addTail(hashTable.buckets[index], newNode);
+}
+
+void printHashTable(HashTable hashTable) {
+    cout << "--- Hash Table --- \n";
+    for (int i = 0; i < MAX_BUCKET; i++)
+    {
+        cout << "Bucket [" << i << "] = [ ";
+        for (Node *p = hashTable.buckets[i].pHead; p != NULL; p = p->pNext)
+        {
+            cout << p->data << "   ";
+        }
+        cout << " ]\n";
+    }
+}
 
 int main(int argc, char const *argv[])
 {
+    HashTable hashTable;
+    initHashTable(hashTable);
 
+    add(hashTable, initNode(50));
+    add(hashTable, initNode(35));
+    add(hashTable, initNode(90));
+    add(hashTable, initNode(36));
+    add(hashTable, initNode(21));
+    add(hashTable, initNode(73));
+    add(hashTable, initNode(28));
+    add(hashTable, initNode(53));
+    add(hashTable, initNode(13));
+    add(hashTable, initNode(64));
+    add(hashTable, initNode(37));
+
+    printHashTable(hashTable);
 
     return 0;
 }
